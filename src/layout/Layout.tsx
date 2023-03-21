577c43e3a16e4caf33069eb3d138c4b0cbe00a45
Joseph Lepley <josephflepley+pro@gmail.com>
2023-03-21 18:04:19 -0400

    Start building layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from './components/Header';
+
+export const Layout = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
---
+++ src/layout/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>
+        Fake Blog
+      </h1>
+    </header>
+  );
+};
