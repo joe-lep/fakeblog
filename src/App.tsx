ae4e4e21548fd5b00c619d8cdee14af1e756c5c6
Joseph Lepley <josephflepley+pro@gmail.com>
2023-03-24 12:22:48 -0400

    Add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import Routing from './Routing';
 
 const queryClient = new QueryClient();
 
+const theme = createTheme();
+
 const App = () => {
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-        <Routing />
+        <ThemeProvider theme={theme}>
+          <Routing />
+        </ThemeProvider>
       </QueryClientProvider>
     </div>
   );
