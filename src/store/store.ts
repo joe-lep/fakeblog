004cbaa82fd16da4655876e353c9af3e45e1bc44
Joseph Lepley <josephflepley+pro@gmail.com>
2023-03-31 12:25:53 -0400

    Remove unused import

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './reducers';
